3d586fb708aafd79135f48eecf394b125911ac73
YoanTsanev <joan.al2001@gmail.com>
2022-05-07 17:06:29 +0300

    5. With command "Open" now we can create and read information, if there is any.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Objects;
@@ -19,10 +23,32 @@ public class Main {
 
             if (action.contains("open")) {
                 String fileName = commandInfo[1];
-                System.out.println("Function Open File");
-                File myFile = new File(fileName);
-                System.out.println(Arrays.toString(commandInfo));
-                System.out.println(fileName);
+               // System.out.println("Function Open File");
+
+                try {
+                    File myFile = new File(fileName);
+                    if (myFile.createNewFile()) {
+                        System.out.println("File created: " + myFile.getName());
+                        System.out.println("");
+                    } else {
+                        System.out.println("File already exists with the following context:");
+                        System.out.println("↓");
+                        Scanner myReader = new Scanner(myFile);
+                        while (myReader.hasNextLine()) {
+                            String data = myReader.nextLine();
+                            System.out.println(data);
+                        }
+                        myReader.close();
+                        System.out.println("↑");
+                        System.out.println("");
+                    }
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+
+               // System.out.println(Arrays.toString(commandInfo)); // ПРОВЕРКА!
+               // System.out.println(fileName);// file path!!!
             }
 
             if (action.contains("help")){
