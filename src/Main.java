0e215a717110a9fc643a0709a8ce8820e165c13e
YoanTsanev <joan.al2001@gmail.com>
2022-05-07 15:32:04 +0300

    1. added command input 2. Added "Exit" features.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner consoleInput = new Scanner(System.in);
-        String command;
+        String command = "";
 
-        System.out.print("Enter command: "); // на същия ред ще иска да се въведе команда
-        command = consoleInput.nextLine();
+        //System.out.print("Enter command: "); // на същия ред ще иска да се въведе команда
+       // command = consoleInput.nextLine();
 
-        System.out.println("The command is: " + command); // печет на резултата
+        while (!Objects.equals(command, "Exit")) {
+            System.out.print("Enter command: ");
+            command = consoleInput.nextLine();
+        }
+
+        System.out.println("Exiting the program..."); // печет за край
     }
 }
