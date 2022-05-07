3c465764e281b3a4a9e4c47733160a89c31e1d16
YoanTsanev <joan.al2001@gmail.com>
2022-05-07 16:28:47 +0300

    4. Created "Help" menu.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Objects;
@@ -17,8 +18,24 @@ public class Main {
             String action = commandInfo[0];
 
             if (action.contains("open")) {
+                String fileName = commandInfo[1];
                 System.out.println("Function Open File");
+                File myFile = new File(fileName);
                 System.out.println(Arrays.toString(commandInfo));
+                System.out.println(fileName);
+            }
+
+            if (action.contains("help")){
+//                System.out.println("---------");
+//                System.out.println("HELP MENU");
+//                System.out.println("---------");
+                System.out.println("The following commands are supported:");
+                System.out.println("Open <file> \t\t opens <file>");
+                System.out.println("Close \t\t\t     closes the currently open file");
+                System.out.println("Save \t\t\t     saves the currently open file");
+                System.out.println("SaveAs <file> \t\t saves the currently open file in <file>");
+                System.out.println("Help \t\t\t     prints this information");
+                System.out.println("Exit \t\t\t     exists the program");
             }
 
             System.out.print("Enter command: ");
