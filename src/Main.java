49bf03b156ee9ed3e17b25ec62728346b5cf46e1
YoanTsanev <joan.al2001@gmail.com>
2022-05-07 16:00:15 +0300

    1. added command input 2. Added "Exit" features. 3. Added experimental "Open" feature.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -10,9 +12,17 @@ public class Main {
         //System.out.print("Enter command: "); // на същия ред ще иска да се въведе команда
        // command = consoleInput.nextLine();
 
-        while (!Objects.equals(command, "Exit")) {
+        while (!Objects.equals(command, "exit")) {
+            String commandInfo[] = command.split(" ");
+            String action = commandInfo[0];
+
+            if (action.contains("open")) {
+                System.out.println("Function Open File");
+                System.out.println(Arrays.toString(commandInfo));
+            }
+
             System.out.print("Enter command: ");
-            command = consoleInput.nextLine();
+            command = consoleInput.nextLine().toLowerCase(Locale.ROOT);// команди с малки букви ще се разчитат
         }
 
         System.out.println("Exiting the program..."); // печет за край
